6fb47630fff3adf59179cb175556a23796b869f3
Karn Worakarn <korkarn.worakrn@gmail.com>
2022-04-29 16:15:27 +0700

    add: generic for get data from api

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { User } from '../models/user';
 
 const URL = "https://jsonplaceholder.typicode.com/";
 
@@ -11,8 +12,8 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getAllUsers(): Observable<any> {
+  getAllUsers(): Observable<User[]> {
     console.log("Called getAllUsers()");
-    return this.http.get(URL + "users");
+    return this.http.get<User[]>(URL + "users");
   }
 }
